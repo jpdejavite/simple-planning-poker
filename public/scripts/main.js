8baad71f5531cd876f460ec52027b88783b7d590
Joao Araujo <jpdejavite@gmail.com>
2020-05-01 19:28:28 -0300

    feat: load deck options from database

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -5,6 +5,16 @@ document.addEventListener('DOMContentLoaded', async () => {
       return;
     }
 
+    const database = firebase.app().database();
+
+    const deckList = ['<option selected disabled value="">Escolha...</option>'];
+    const decksSnap = await database.ref(`/decks`).once('value');
+    decksSnap.forEach((deckSnap) => {
+      const deck = deckSnap.val();
+      deckList.push(`<option id="${deckSnap.key}">${deck.name} - ${deck.cards.join(',')}</option>`);
+    });
+    document.getElementById('sessionDeck').innerHTML = deckList.join('');
+  
     const matches = (/\/session\/(.+)/i).exec(window.location.pathname);
     if (matches && matches.length > 1) {
       initSession({ sessionId: matches[1] });
